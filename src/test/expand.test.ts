6b2616e40a9ca63399d3493fdbf460d551dbd7d4
Raymond Zhao <raymondzhao@microsoft.com>
2020-10-09 12:39:58 -0700

    Add test for CSS d to display:block

--- src/test/expand.test.ts
+++ src/test/expand.test.ts
@@ -9,36 +9,36 @@ const COMPLETE_OPTIONS = {
 	showAbbreviationSuggestions: false,
 	syntaxProfiles: {},
 	variables: {}
-} 
+}
 
 describe('Expand Abbreviations', () => {
-  
-  function testExpand(syntax: string, abbrev: string, expanded: string) {
+	function testExpand(syntax: string, abbrev: string, expanded: string) {
 		it(`should expand ${abbrev} to ${expanded}`, async () => {
 			const document = TextDocument.create(`test://test/test.${syntax}`, syntax, 0, abbrev);
 			const position = Position.create(0, abbrev.length);
 
 			const completionList = doComplete(document, position, syntax, COMPLETE_OPTIONS);
 
-			assert.ok(completionList && completionList.items, `completion list exists for ${abbrev}`)
-			assert.ok(completionList.items.length > 0, `completion list is not empty for ${abbrev}`)
-			
-			assert.equal(expanded, TextDocument.applyEdits(document, [completionList.items[0].textEdit]))
+			assert.ok(completionList && completionList.items, `completion list exists for ${abbrev}`);
+			assert.ok(completionList.items.length > 0, `completion list is not empty for ${abbrev}`);
+
+			assert.equal(expanded, TextDocument.applyEdits(document, [completionList.items[0].textEdit]));
 		})
 	}
-	
+
 	function testNotExpand(syntax: string, abbrev: string) {
 		it(`should not expand ${abbrev}`, async () => {
 			const document = TextDocument.create(`test://test/test.${syntax}`, syntax, 0, abbrev);
 			const position = Position.create(0, abbrev.length);
 
 			const completionList = doComplete(document, position, syntax, COMPLETE_OPTIONS);
-			
-			assert.ok(!completionList)
+
+			assert.ok(!completionList);
 		})
 	}
-	
-	testExpand('jsx', 'button[onClick={props.onClick}]', '<button onClick="props.onClick">${0}</button>')
 
-	testNotExpand('html', 'div*101')
+	testExpand('jsx', 'button[onClick={props.onClick}]', '<button onClick="props.onClick">${0}</button>');
+	testExpand('css', 'd', 'display: ${2:block};');
+
+	testNotExpand('html', 'div*101');
 })
